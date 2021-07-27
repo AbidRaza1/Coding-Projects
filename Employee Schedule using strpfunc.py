155c59e79db510ae9ccd21e9ed07fc339e67afad
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-27 14:28:40 -0600

    Update Employee Schedule using strpfunc.py

--- Employee Schedule using strpfunc.py
+++ Employee Schedule using strpfunc.py
@@ -1,5 +1,8 @@
+#Importing datetime library
 
 import datetime 
+
+# Asking user to input employees name and their scheduling dates
 
 Emp1 = input('Please enter name of employees to work in Bakery').upper()
 Emp2 = input('Please enter name of employees to work at FrontEnd').upper()
@@ -9,10 +12,18 @@ Emp5 = input('Please enter name of employees to work on FryerStation').upper()
 Emp6 = input('Please enter name of employees to work in CashHandling').upper()
 todaydate = input('Please enter the schedule start date(mm/dd/yyyy)')
 enddate =   input('Please enter the schedule end date(mm/dd/yyyy)')
-current  = datetime.datetime.strptime(todaydate, '%m/%d/%Y')
 
+# Formatting date time object using strptime() function
+
+current  = datetime.datetime.strptime(todaydate, '%m/%d/%Y')
 end = datetime.datetime.strptime(enddate, '%m/%d/%Y')
+
+# Calculating schedule days
+
 scheduledays = end  -  current
+
+# Printing Schedule and Depatrment and in which each employee will be deployed
+
 print('Following is the schedule for next' + ' ' + str(scheduledays.days)+ 'days \n')
 print(str(Emp1) + ' ' + 'is going to work in Bakery \n')
 print(str(Emp2) + ' ' + 'is going to work in FrontEnd \n')
