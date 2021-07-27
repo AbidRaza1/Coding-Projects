b6d05ffce1c9b132302e0af11475c4b9b6dcc0d7
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-27 15:53:21 -0600

    Sum of six natural numbers using FOR

--- Sum of first Six natural numbers using FOR loop.py
+++ Sum of first Six natural numbers using FOR loop.py
@@ -1,5 +1,10 @@
+
+#Initializaing counter variable
 x = 0
+
+#For loop with range() function
 for i in range(1,7):
                     x =  x+i
                     y = x
-print(y)
+#Printing results
+ print(y)
