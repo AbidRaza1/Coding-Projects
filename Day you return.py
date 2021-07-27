22471540abaa7d7552952ec8e3fd19fdc87df37e
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-27 15:02:57 -0600

    Update Day you return.py

--- Day you return.py
+++ Day you return.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
 
 
-#Day you will return
-
-
+# Calculating the return date.
 
 calender = print('Calendar days:','Sudnay-Saturday:', '0-6')
 
+# Asking user to enter the day he wants to leave
+
 day = int(input('Please enter number of day you are planning to leave from calender '))
+
+# Asking user to enter the number of days for leave
 
 length = int(input('How long would you like to stay(No.of days) '))
 
+# Calculating return day 
+
 t = (day + length) % 7
 
+# Printing results
+
 print('You will return on ', t, "calenday day of the week")
-
-
-
-
-
-# In[ ]:
-
-
-
-
