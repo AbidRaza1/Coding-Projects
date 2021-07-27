06ff5c6b5f5ad726f53ba14dd543284aaf8876d2
Abid Raza <45925346+AbidRaza1@users.noreply.github.com>
2021-07-27 14:33:24 -0600

    Update Grocery Counter.py

--- Grocery Counter.py
+++ Grocery Counter.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# ## GROCERY COUNTER
 
-# In[6]:
+# GROCERY COUNTER
 
+
+# Initializing loop
 
 nitems = 0
 ncount =0
 more = True
+
+# Checking for more items
+
 while more:
             x = float(input('Enter item price or zero if no item is left: '))
             if x > 0 and x != 0:
@@ -16,13 +18,10 @@ while more:
                     ncount =  1 + ncount
             else:
                  break
+# Printing price and items
+
 print('Total Price',nitems)
 print('Total Items',ncount)
-            
-
-
-# In[ ]:
-
 
 
 
